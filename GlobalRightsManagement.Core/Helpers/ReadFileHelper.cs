0c53b086763eaef39fa40a515159abf93b59be98
olman.cordero <ocormen02@gmail.com>
2025-09-28 13:32:08 -0600

    removed references

--- GlobalRightsManagement.Core/Helpers/ReadFileHelper.cs
+++ GlobalRightsManagement.Core/Helpers/ReadFileHelper.cs
@@ -1,10 +1,4 @@
-﻿using GlobalRightsManagement.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace GlobalRightsManagement.Core.Helpers
 {
